53e8e9633dace524d84f527dda0b7565bbc8dc30
GabrielLinharesRamos <112952323+GabrielLinharesRamos@users.noreply.github.com>
2024-01-23 01:35:35 -0300

    lucio,me salva aqui mash

--- spotify_front/src/components/Private.js
+++ spotify_front/src/components/Private.js
@@ -1,5 +1,5 @@
-import { useState,useEffect } from "react"
-import { Navigate } from "react-router-dom"
+// import { useState,useEffect } from "react"
+// import { Navigate } from "react-router-dom"
 
 export default function Private({children}){
 
